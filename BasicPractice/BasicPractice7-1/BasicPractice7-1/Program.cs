c9419224205ef5de548630ad024089f0243f8f97
Kayxue <s0990006@gmail.com>
2022-11-18 00:07:02 +0800

    2022/11/17 Finished BasicPractice7-1

---
+++ BasicPractice/BasicPractice7-1/BasicPractice7-1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int GCD(int a, int b)
+{
+    if (b == 0) return a;
+    return GCD(b, a % b);
+}
+
+int repeatTimes;
+Console.Write("How many sets of test data: ");
+repeatTimes = int.Parse(Console.ReadLine());
+for (int i = 0; i < repeatTimes; i++)
+{
+    int a, b;
+    Console.Write("\na = ");
+    a = int.Parse(Console.ReadLine());
+    Console.Write("b = ");
+    b = int.Parse(Console.ReadLine());
+    Console.WriteLine($"GCD({a}, {b}) = {GCD(a, b)}");
+}
