e4aa6f5be137f86181072befa65f783ef9a2ba9f
Kayxue <s0990006@gmail.com>
2022-11-18 00:18:49 +0800

    2022/11/17 Finished BasicPractice7-2

---
+++ BasicPractice/BasicPractice7-2/BasicPractice7-2/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int finalIndex;
+int nowA = 0;
+void mya(int k, int m, int n, int s, int t)
+{
+    if (k > finalIndex) return;
+    Console.WriteLine($"a_{k} = {nowA}");
+    nowA = k % 2 == 0 ? m * nowA + n : s * nowA + t;
+    mya(k + 1, m, n, s, t);
+}
+
+int repeatTimes;
+Console.Write("How many sets of test data: ");
+repeatTimes = int.Parse(Console.ReadLine());
+for (int i = 0; i < repeatTimes; i++)
+{
+    int m, n, s, t;
+    Console.Write("\nm = ");
+    m = int.Parse(Console.ReadLine());
+    Console.Write("n = ");
+    n = int.Parse(Console.ReadLine());
+    Console.Write("s = ");
+    s = int.Parse(Console.ReadLine());
+    Console.Write("t = ");
+    t = int.Parse(Console.ReadLine());
+    Console.Write("Final index = ");
+    finalIndex = int.Parse(Console.ReadLine());
+    mya(0, m, n, s, t);
+    nowA = 0;
+}
