77692462e4c04ed2fe79304beefea4bb4631c10a
Kayxue <s0990006@gmail.com>
2022-11-18 12:03:57 +0800

    2022/11/18 Finished Practice10-2

---
+++ Practice/Practice10-2/Practice10-2/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+unsafe void strangeFunc(int[] a)
+{
+    fixed (int* p1 = a, q2 = &a[a.Length - 1])
+    {
+        int* p = p1;
+        int* q = q2;
+        while (p < q && p != q)
+        {
+            while (*p >= 0)
+            {
+                if (p >= q) return;
+                p += 1;
+            }
+
+            while (*q >= 0)
+            {
+                if (p >= q) return;
+                q -= 1;
+            }
+            swap(ref *p, ref *q);
+            p += 1;
+            q -= 1;
+        }
+    }
+}
+
+void swap(ref int a, ref int b)
+{
+    (a, b) = (b, a);
+}
+
+void printArray(bool after, string name, int[] arr)
+{
+    Console.Write($"{(after?"後來":"原先")} {name}[] 為 {{{arr[0]}");
+    for (int i = 1; i < arr.Length; i++)
+    {
+        Console.Write($", {arr[i]}");
+    }
+    Console.Write("}\n");
+}
+
+int[] a = { 65, 0, -5, -22, 9, 231, 43, -7, 11, -65, 0, -18, 65 },
+    b = { 65, 0, -5, -22, 9, 231, 43, -7, 11, 65, 0, -18 },
+    c = { 65, 0, 5, 22, 9, 231, 43, 7, 11, 65, 0, 18 };
+
+printArray(false,"a",a);
+printArray(false,"b",b);
+printArray(false,"c",c);
+
+strangeFunc(a);
+strangeFunc(b);
+strangeFunc(c);
+
+printArray(true,"a",a);
+printArray(true,"b",b);
+printArray(true,"c",c);
---
+++ Practice/Practice10-2/README.md
@@ -0,0 +1,43 @@
+﻿# 第十次第二題
+## 題目
+Write a function `strangeFunc()` whose prototype is:
+
+```c++
+void strangeFunc(int a[], int size);
+```
+
+This function will reverse the locations of negative numbers in the array `a[]`.  
+You must use two pointers p and q to do the work as depicted in the following figures.  
+Test your main() funtion as follow,
+```c++
+int main() {
+    int a[13] = {65, 0, -5, -22, 9, 231, 43, -7, 11, -65, 0, -18, 65};
+    int b[12] = {65, 0, -5, -22, 9, 231, 43, -7, 11, 65, 0, -18};
+    int c[12] = {65, 0, 5, 22, 9, 231, 43, 7, 11, 65, 0, 18};
+
+    print the original list in a[];
+    print the original list in b[];
+    print the original list in c[];
+
+    strangeFunc(a, 13);
+    strangeFunc(b...);
+    strangeFunc(c...);
+
+    print the modified list in a[];
+    print the modified list in b[];
+    print the modified list in c[];
+
+    return 0;
+}
+```
+## 範例
+```
+
+原先 a[] 為 {65, 0, -5, -22, 9, 231, 43, -7, 11, -65, 0, -18, 65}
+原先 b[] 為 {65, 0, -5, -22, 9, 231, 43, -7, 11, 65, 0, -18}
+原先 c[] 為 {65, 0, 5, 22, 9, 231, 43, 7, 11, 65, 0, 18}
+
+後來 a[] 為 {65, 0, -18, -65, 9, 231, 43, -7, 11, -22, 0, -5, 65}
+後來 b[] 為 {65, 0, -18, -7, 9, 231, 43, -22, 11, 65, 0, -5}
+後來 c[] 為 {65, 0, 5, 22, 9, 231, 43, 7, 11, 65, 0, 18}
+```
