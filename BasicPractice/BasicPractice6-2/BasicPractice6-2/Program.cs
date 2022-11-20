cfea2aa857780255d1223dc95ca1f65f174302f3
Kayxue <s0990006@gmail.com>
2022-11-20 18:37:37 +0800

    2022/11/20 Finished BasicPractice6-2

---
+++ BasicPractice/BasicPractice6-2/BasicPractice6-2/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string input = Console.ReadLine();
+foreach (char c in input)
+{
+    Console.WriteLine($"[{c}] {(int)c} ==> [{(char)(c + 1)}] {c+1}");
+}
