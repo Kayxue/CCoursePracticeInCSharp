7dccaa5ad4c2762bc5845bfa25ab2ec6de2bd2e5
Kayxue <s0990006@gmail.com>
2022-11-20 19:18:39 +0800

    2022/11/20

---
+++ BasicPractice/BasicPractice5-1/BasicPractice5-1/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] endDate = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+bool isLeapYear(int year)
+{
+    return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
+}
+
+bool isInvalidDate(int year, int month, int day)
+{
+    if (month is < 1 or > 12 || day < 1) return true;
+    return day > (month == 2 ? isLeapYear(year) ? 29 : 28 : endDate[month - 1]);
+}
+
+int repeatTime;
+Console.Write("How many sets of test data: ");
+repeatTime = int.Parse(Console.ReadLine());
+for (int i = 0; i < repeatTime; i++)
+{
+    string input;
+    int[] arg = new int[3];
+    Console.Write("\nInput a date (year/month/day): ");
+    input = Console.ReadLine();
+    arg = input.Split("/").Select(e => int.Parse(e)).ToArray();
+    Console.WriteLine($"{input} is {(isInvalidDate(arg[0], arg[1], arg[2]) ? "not " : "")}a valid date.");
+}
