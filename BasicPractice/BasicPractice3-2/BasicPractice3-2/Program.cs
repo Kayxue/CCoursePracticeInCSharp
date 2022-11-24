c8026c20b21e66d6596704a0e7342ec4d37c0666
Kayxue <s0990006@gmail.com>
2022-11-24 22:09:03 +0800

    2022/11/24 Finish BasicPractice3-2

---
+++ BasicPractice/BasicPractice3-2/BasicPractice3-2/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int studentCount;
+int[] monthCount = new int[12];
+Console.Write("請問有幾位同學？");
+studentCount = int.Parse(Console.ReadLine());
+Console.WriteLine("請輸入他們的生日 (格式為 年/月/日)。");
+for (int i = 0; i < studentCount; i++)
+{
+    string input = Console.ReadLine();
+    int month = int.Parse(input.Split("/")[1]);
+    monthCount[month - 1] += 1;
+}
+for (int i = 0; i < 12; i++)
+{
+    Console.WriteLine($"{i + 1}月出生的有 {monthCount[i]} 位");
+}
