bbecd734787b63225c2229918ae87e8d61f0a0a5
Kayxue <s0990006@gmail.com>
2022-11-24 20:55:44 +0800

    2022/11/24 Finished BasicPractice9-2

---
+++ BasicPractice/BasicPractice9-2/BasicPractice9-2/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int repeatTime;
+Console.Write("How many sets of test data: ");
+repeatTime = int.Parse(Console.ReadLine());
+for (int i = 0; i < repeatTime; i++)
+{
+    string input;
+    int skipping;
+    Console.Write("\nInput a string: ");
+    input = Console.ReadLine();
+    Console.Write("Skipping = ");
+    skipping = int.Parse(Console.ReadLine());
+    Console.WriteLine($"[{input}]");
+    while (input.Length > skipping)
+    {
+        input = input.Substring(skipping);
+        Console.WriteLine($"[{input}]");
+    }
+}
