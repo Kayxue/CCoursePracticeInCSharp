e67f00dbb06623a06e8a3cb76035531ce92e32c0
Kayxue <s0990006@gmail.com>
2022-11-24 20:42:38 +0800

    2022/11/24 Finished BasicPractice9-1

---
+++ BasicPractice/BasicPractice9-1/BasicPractice9-1/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int repeatTimes;
+Console.Write("How many sets of test data: ");
+repeatTimes = int.Parse(Console.ReadLine());
+for (int i = 0; i < repeatTimes; i++)
+{
+    Console.WriteLine("\nInput a line:");
+    string input = Console.ReadLine();
+    Console.WriteLine("Modified line:");
+    foreach (char c in input)
+    {
+        if (!((c >= '0' && c <= '9') || c == '.' || c == '%'))
+        {
+            Console.Write("_");
+        }
+        else
+        {
+            Console.Write(c);
+        }
+    }
+    Console.Write("\n");
+}
