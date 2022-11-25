9b0c572ae2cf41e354cbeed4cf938e10d4f8f8fc
Kayxue <s0990006@gmail.com>
2022-11-25 16:35:14 +0800

    2022/11/25 Finished Practice11-1

---
+++ Practice/Practice11-1/Practice11-1/Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int indexOfList(string str, List<string> list)
+{
+    return list.IndexOf(str);
+}
+
+List<string> country =
+    new List<string>
+    {
+        "China",
+        "Japan",
+        "Korea",
+        "India",
+        "Indonesia",
+        "Malaysia",
+        "Singapore",
+        "Thailand",
+        "Viet Nam",
+        "Taiwan",
+        "Mexico",
+        "Brazil",
+        "Canada",
+        "USA",
+        "the United Kingdom",
+        "Italy",
+        "Spain",
+        "France",
+        "Germany",
+        "Australia",
+        "New Zealand"
+    };
+
+List<int> population = new List<int>
+{
+    1425887, 123952, 51816, 1417173, 275501,
+    33938, 5976, 71697, 98187, 23893, 127504,
+    215313, 38454, 338290, 67509, 59037, 47559,
+    64627, 83370, 26177, 5185
+};
+
+string input = "";
+Console.WriteLine("Welcome to the World Population Database!");
+while (true)
+{
+    Console.Write("\nInput a country (\"quit\" to exit): ");
+    input = Console.ReadLine();
+    if (input == "quit")
+    {
+        break;
+    }
+    int index = indexOfList(input, country);
+    switch (index)
+    {
+        case -1:
+            Console.WriteLine($"Sorry, we cannot find [{input}] in our database.");
+            break;
+        default:
+            Console.WriteLine($"The population of {country[index]} is {population[index]} thousands.");
+            break;
+    }
+}
+Console.WriteLine("\nThank you for using World Population Database!");
+
---
+++ Practice/Practice11-1/README.md
@@ -0,0 +1,38 @@
+﻿# 第十一次第一題
+## 題目
+Write a function to search a string in a list and returns its index (-1 if not found).
+
+```c++
+int indexOfList(char str[], char *list[], int itemTotal);
+```
+
+where `str` is the target string, `list` is an array of strings, and `itemTotal` is the number of strings in `list`.
+At the beginning of your programming, test `indexOfList()` as follows.
+
+```
+Input a line ("quit" to exit): the United Kingdom
+Its index is 14.
+Input a line ("quit" to exit): cjlin
+It is not in the list.
+Input a line ("quit" to exit): quit
+```
+
+Write a program to provide a database of populations of countries.
+
+## 範例
+```
+Welcome to the World Population Database!
+
+Input a country ("quit" to exit): the United Kingdom
+The population of the United Kingdom is 67509 thousands.
+
+Input a line ("quit" to exit): www
+Sorry, we cannot find [www] in our database.
+
+Input a country ("quit" to exit): Singapore
+The population of Singapore is 5976 thousands.
+
+Input a line ("quit" to exit): quit
+
+Thank you for using World Population Database!
+```
